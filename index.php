e97bea3991c29a0cac77866a20c01fd772fd45d6
atolentino07 <102979320+atolentino07@users.noreply.github.com>
2023-09-22 23:19:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Hydro Science</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/water.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NewBiz - v4.7.0
+  * Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+      
+        <h1><a href="index.html">Hydro Science</a></h1>
+        
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.html">Home</a></li>
+          <li><a href="types.html">Types</a></li>
+          <li><a href="issues.html">Issues</a></li>
+          <li><a href="effects.html">Effects</a></li>
+          <li><a href="govacts.html">Government Acts</a></li> 
+          <li><a href="solutions.html">Solutions</a></li>
+		  <li><a  href="aboutus.html">About us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- #header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="clearfix">
+    <div class="container" data-aos="fade-up">
+	<div class="hero-img" data-aos="zoom-out" data-aos-delay="200">
+	
+        <img src="assets/img/pollution.png" alt="" class="img-fluid">
+      </div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+
+      <div class="hero-info" data-aos="zoom-in" data-aos-delay="100">
+	  
+        <h2>What is <span>water pollution</span>?</h2>
+		
+		<p style="color: white;">
+				Water pollution is the contamination of water sources by substances which make the water unusable for drinking, cooking, cleaning, swimming, and other activities. <br>
+				Pollutants include chemicals, trash, bacteria, and parasites. 
+				All forms of pollution eventually make their way to water. Air pollution settles onto lakes and oceans. 
+				Land pollution can seep into an underground stream, then to a river, and finally to the ocean. 
+				Thus, waste dumped in a vacant lot can eventually pollute a water supply.
+		  </p>
+		  <br>
+		  <p><a style="color: white;" href="types.html">Click here to learn more</a></p>
+		  <br>
+		  <br>
+		  <p></p>
+		  
+      </div>
+
+    </div>
+  </section><!-- End Hero Section -->
+  
+
+  
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer"> 
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Hydro Science</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+        All the links in the footer should remain intact.
+        You can delete the links only if you purchased the pro version.
+        Licensing information: https://bootstrapmade.com/license/
+        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
+      -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
